ab48f96fd92285aef28d0838d25191ee78dc480f
sachinsharma92 <sachindesigner2018@gmail.com>
2022-11-28 22:30:57 +0530

    FIX: error for build

--- pages/privacy/index.js
+++ pages/privacy/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import BuildGreatBanner from '../../common/buildGreatBanner';
 import LayoutPrimary from '../../common/LayoutPrimary'
 import PrivacyPolicy from '../../components/privacyPolicy';
-import OurServices from '../../components/servicesPage/OurServices';
 
 export default function PrivacyPolicyPage() {
   return (
