68d1dd641d845fa2f16dd0c55232701826dfefb1
sachinsharma92 <sachindesigner2018@gmail.com>
2022-11-28 18:10:37 +0530

    FIX: how we work section

--- components/homepage/howWeWork/index.js
+++ components/homepage/howWeWork/index.js
@@ -1,12 +1,72 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import style from "./style.module.scss"
-import { Col, Nav, Row, Tab } from 'react-bootstrap';
 import Link from 'next/link';
 import { FaArrowRight } from 'react-icons/fa';
 import gsap from 'gsap';
 
 export default function HowWeWork() {
+  const [count, setCount] = useState(5);
+
+  const [activeTitle, setActiveTitle] = useState(1)
+
+  const dataList = [
+    {
+      imgUrl: "/images/services/discover.jpeg",
+      title: "Discover",
+      description: "Lorem ipsum dolor sit amet consectetur. Magna tincidunt aliquet id ut convallis adipiscing sit. Mus ut amet eget senectus posuere. Quam nunc elementum ultricies eget vel.",
+      id: 1,
+    },
+    {
+      imgUrl: "/images/services/design.webp",
+      title: "Design",
+      description: "Lorem ipsum dolor sit amet consectetur. Magna tincidunt aliquet id ut convallis adipiscing sit. Mus ut amet eget senectus posuere. Quam nunc elementum ultricies eget vel.",
+      id: 2,
+    },
+    {
+      imgUrl: "/images/services/development.jpeg",
+      title: "Development",
+      description: "Lorem ipsum dolor sit amet consectetur. Magna tincidunt aliquet id ut convallis adipiscing sit. Mus ut amet eget senectus posuere. Quam nunc elementum ultricies eget vel.",
+      id: 3
+    },
+    {
+      imgUrl: "/images/services/testing.webp",
+      title: "Testing",
+      description: "Lorem ipsum dolor sit amet consectetur. Magna tincidunt aliquet id ut convallis adipiscing sit. Mus ut amet eget senectus posuere. Quam nunc elementum ultricies eget vel.",
+      id: 4
+    },
+    {
+      imgUrl: "/images/services/deployment.webp",
+      title: "Deployment",
+      description: "Lorem ipsum dolor sit amet consectetur. Magna tincidunt aliquet id ut convallis adipiscing sit. Mus ut amet eget senectus posuere. Quam nunc elementum ultricies eget vel.",
+      id: 5
+    },
+    {
+      imgUrl: "/images/services/support.webp",
+      title: "Support",
+      description: "Lorem ipsum dolor sit amet consectetur. Magna tincidunt aliquet id ut convallis adipiscing sit. Mus ut amet eget senectus posuere. Quam nunc elementum ultricies eget vel.",
+      id: 6
+    },
+  ];
+
+
+  const [imageSource, setImageSource] = useState(dataList[0].imgUrl);
+  const [animationActive, setAnimationActive] = useState(false);
+
+  useEffect(() => {
+    let ImageSourceInterval;
+    if (count < dataList.length) {
+      ImageSourceInterval = setInterval(() => setCount(count + 1), 2500);
+
+    }
+    if (count >= dataList.length) {
+      setCount(0);
+    }
+
+    setImageSource(dataList[count]?.imgUrl);
+    setActiveTitle(dataList[count]?.id)
+    return () => clearInterval(ImageSourceInterval);
+  }, [count]);
 
 
   useEffect(() => {
@@ -30,6 +90,8 @@ export default function HowWeWork() {
 
 
 
+
+
   return (
     <section id='howWeWork' className={`${style.howWeWork} tab-vertical-image`}>
       <div className="container">
@@ -45,157 +107,39 @@ export default function HowWeWork() {
             </a>
           </Link>
         </div>
-        <div className={style.tabSection}>
-          <Tab.Container defaultActiveKey="discover">
-            <Row>
-              <Col sm={5}>
-                <Nav variant="pills" className="flex-column">
-                  <Nav.Item>
-                    <Nav.Link eventKey="discover">
-                      <h3 className="title3">Discover</h3>
-                      <p className="text4 line3">Lorem ipsum dolor sit amet consectetur. Magna tincidunt aliquet id ut convallis adipiscing sit. Mus ut amet eget senectus posuere. Quam nunc elementum ultricies eget vel.</p>
 
-                      <div className={`${style.imgBox} d-sm-none img-mobile`}>
-                        <Image
-                          src="/images/services/discover.jpeg"
-                          alt="Picture of the author"
-                          layout="fill"
-                        />
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="design">
-                      <h3 className="title3">Design</h3>
-                      <p className="text4 line3">Lorem ipsum dolor sit amet consectetur. Magna tincidunt aliquet id ut convallis adipiscing sit. Mus ut amet eget senectus posuere. Quam nunc elementum ultricies eget vel.</p>
-
-                      <div className={`${style.imgBox} d-sm-none img-mobile`}>
-                        <Image
-                          src="/images/services/design.webp"
-                          alt="Picture of the author"
-                          layout="fill"
-                        />
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="development">
-                      <h3 className="title3">Development</h3>
-                      <p className="text4 line3">Lorem ipsum dolor sit amet consectetur. Magna tincidunt aliquet id ut convallis adipiscing sit. Mus ut amet eget senectus posuere. Quam nunc elementum ultricies eget vel.</p>
-
-                      <div className={`${style.imgBox} d-sm-none img-mobile`}>
-                        <Image
-                          src="/images/services/development.jpeg"
-                          alt="Picture of the author"
-                          layout="fill"
-                        />
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="testing">
-                      <h3 className="title3">Testing</h3>
-                      <p className="text4 line3">Lorem ipsum dolor sit amet consectetur. Magna tincidunt aliquet id ut convallis adipiscing sit. Mus ut amet eget senectus posuere. Quam nunc elementum ultricies eget vel.</p>
-
-                      <div className={`${style.imgBox} d-sm-none img-mobile`}>
-                        <Image
-                          src="/images/services/testing.webp"
-                          alt="Picture of the author"
-                          layout="fill"
-                        />
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="deployment">
-                      <h3 className="title3">Deployment</h3>
-                      <p className="text4 line3">Lorem ipsum dolor sit amet consectetur. Magna tincidunt aliquet id ut convallis adipiscing sit. Mus ut amet eget senectus posuere. Quam nunc elementum ultricies eget vel.</p>
-
-                      <div className={`${style.imgBox} d-sm-none img-mobile`}>
-                        <Image
-                          src="/images/services/deployment.webp"
-                          alt="Picture of the author"
-                          layout="fill"
-                        />
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="support">
-                      <h3 className="title3">Support</h3>
-                      <p className="text4 line3">Lorem ipsum dolor sit amet consectetur. Magna tincidunt aliquet id ut convallis adipiscing sit. Mus ut amet eget senectus posuere. Quam nunc elementum ultricies eget vel.</p>
-
-                      <div className={`${style.imgBox} d-sm-none img-mobile`}>
-                        <Image
-                          src="/images/services/support.webp"
-                          alt="Picture of the author"
-                          layout="fill"
-                        />
-                      </div>
-                    </Nav.Link>
-                  </Nav.Item>
-                </Nav>
-              </Col>
-              <Col sm={7} className="d-none d-sm-block">
-                <Tab.Content>
-                  <Tab.Pane eventKey="discover">
-                    <div className={style.imgBox}>
-                      <Image
-                        src="/images/services/discover.jpeg"
-                        alt="Picture of the author"
-                        layout="fill"
-                      />
-                    </div>
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="design">
-                    <div className={style.imgBox}>
-                      <Image
-                        src="/images/services/design.webp"
-                        alt="Picture of the author"
-                        layout="fill"
-                      />
-                    </div>
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="development">
-                    <div className={style.imgBox}>
-                      <Image
-                        src="/images/services/development.jpeg"
-                        alt="Picture of the author"
-                        layout="fill"
-                      />
-                    </div>
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="testing">
-                    <div className={style.imgBox}>
-                      <Image
-                        src="/images/services/testing.webp"
-                        alt="Picture of the author"
-                        layout="fill"
-                      />
-                    </div>
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="deployment">
-                    <div className={style.imgBox}>
-                      <Image
-                        src="/images/services/deployment.webp"
-                        alt="Picture of the author"
-                        layout="fill"
-                      />
-                    </div>
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="support">
-                    <div className={style.imgBox}>
-                      <Image
-                        src="/images/services/support.webp"
-                        alt="Picture of the author"
-                        layout="fill"
-                      />
-                    </div>
-                  </Tab.Pane>
-                </Tab.Content>
-              </Col>
-            </Row>
-          </Tab.Container>
+        <div className="row mt-5 custom-row">
+          <div className="col-sm-7" id="contentSection">
+            <ul className={style.howWeWorkSection}>
+              {dataList.map((data, index) => (
+                <li
+                  key={index}
+                  className={`list ${data.id === activeTitle ? 'active' : ''}`}
+                  id={`item${index + 1}`}
+                >
+                  <div className={`title3 ${style.titleStyle} ${data.id === activeTitle ? style.activeText : ''}`}>{data.title}</div>
+                  {data.id === activeTitle ? <p className="text4 line3">{data.description}</p> : null}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="col-sm-5">
+            <div className={`${style.imageSection}`} id="imageSection">
+              {imageSource && (
+                <div className={`${style.imgBox}`}>
+                  <Image
+                    key={imageSource}
+                    src={imageSource}
+                    alt="Picture of the author"
+                    layout="fill"
+                    className={`${style.imgStyle}`}
+                    data-aos="zoom-in"
+                    data-aos-duration="1000"
+                  />
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </section>
